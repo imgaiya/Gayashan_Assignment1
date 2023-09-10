af59aef0b02d5ab6dddb755e2d69929737c75d81
Gayashan <gayashan333madhuka99@gmail.com>
2023-09-10 14:11:53 +1000

    get user input assignmentname

---
+++ Assesment_1/AssignmentStatistics.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Write a description of class AssignmentStatistics here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class AssignmentStatistics
+{
+    public static void main(String[] args) {
+
+        //Get user inputs
+        Scanner scanner = new Scanner(System.in);
+        String assignmentName = scanner.nextLine();
+
+        //Input assignment name
+        System.out.print("Enter the assignment name here: ");
+
+    }
+}
