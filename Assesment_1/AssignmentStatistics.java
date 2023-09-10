1a691560141b4730d456d0ac9f72464dababfa2a
Gayashan <gayashan333madhuka99@gmail.com>
2023-09-10 14:20:14 +1000

    Declaring the marks range

--- Assesment_1/AssignmentStatistics.java
+++ Assesment_1/AssignmentStatistics.java
@@ -17,5 +17,18 @@ public class AssignmentStatistics
         //Input assignment name
         System.out.print("Enter the assignment name here: ");
 
+    
+     int[] marks = new int[30];
+        for (int i = 0; i < 30; i++) {
+            int mark = -1; // Invalid mark error massage 
+            while (mark < 0 || mark > 30) {
+                System.out.print("Enter student mark " + (i + 1) + ": ");
+                mark = scanner.nextInt();
+                if (mark < 0 || mark > 30) {
+                    System.out.println("You have entered an invalid mark. Please enter a mark between 0 and 30.");
+                } // Reminder for the marks range
+            }
+            marks[i] = mark;
+        }
     }
 }
